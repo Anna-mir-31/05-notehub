fd763e17308f2d9afb542637025f1b7b2ca47964
Anna_Chernobrovenko <annanyuska2404@gmail.com>
2025-07-26 19:21:06 +0200

    fix

--- src/types/note.ts
+++ src/types/note.ts
@@ -1,7 +1,7 @@
 export type NoteTag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
 
 export interface Note {
-  id: number; // має бути number!
+  id: number; 
   title: string;
   content: string;
   tag: NoteTag;
